ac4b4f95145b03acf1eb05529f170508662084b7
Eleonora Kazakova <abei-2005@yandex.ru>
2022-03-10 13:44:34 +0100

    Create form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,29 +1,78 @@
+import { useState } from "react";
 import "../styles/Contact.css";
 import HeroContact from "./HeroContact";
+import Map from "../pictures/map.jpg";
 
 export default function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const toggleBook = () => {
+    if (
+      name.length > 0 &&
+      time.length > 0 &&
+      date.length > 0 &&
+      email.length > 0
+    ) {
+      setName("");
+      setEmail("");
+      setDate("");
+      setTime("");
+      return alert("We got your booking");
+    }
+  };
   return (
-    <section>
+    <section className="contact-content text">
       <HeroContact />
-      <h2>Opening hours</h2>
-      <p>Monday to Friday08:00 - 20:00 </p>
-      <p>Saturdays09:00 - 22:00 </p>
-      <p>Sundays10:00 - 22:00 </p>
-      <p>Address Ursviksvägen 28, 172 76 Sundbyberg</p>
+      <div className="contact-text-form">
+        <div>
+          <h2>Opening hours</h2>
+          <p>Monday to Friday08: 00 - 20:00 </p>
+          <p>Saturdays 09:00 - 22:00 </p>
+          <p>Sundays 10:00 - 22:00 </p>
+          <p>Address: Gamla torget 5, 753 20 Uppsala</p>
+        </div>
+        <form className="contact-form">
+          <label> Your Full Name</label>
+          <input
+            required
+            type="text"
+            value={name}
+            placeholder="David"
+            onChange={(name) => setName(name.target.value)}
+          ></input>
 
-      <form>
-        <label> Your Full Name</label>
-        <input type="text"></input>
+          <label> Your Email address</label>
+          <input
+            required
+            type="email"
+            value={email}
+            placeholder="david@gmail.com"
+            onChange={(email) => setEmail(email.target.value)}
+          ></input>
 
-        <label> Your Email address</label>
-        <input type="text"></input>
+          <label> Choose date</label>
+          <input
+            required
+            type="date"
+            value={date}
+            onChange={(date) => setDate(date.target.value)}
+          ></input>
 
-        <label> Choose date</label>
-        <input type="date"></input>
-
-        <label> Choose time</label>
-        <input type="time"></input>
-      </form>
+          <label> Choose time</label>
+          <input
+            required
+            type="time"
+            value={time}
+            onChange={(time) => setTime(time.target.value)}
+          ></input>
+          <button className="contact-button" onClick={() => toggleBook()}>
+            Book
+          </button>
+        </form>
+      </div>
+      <img src={Map} className="contact-image" />
     </section>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,18 @@
 import "../styles/Footer.css";
+import LogoInstagram from "../pictures/instagram-brands.svg";
+import LogoFacebook from "../pictures/facebook-brands.svg";
+import { ExternalLink } from "react-external-link";
 
 export default function Footer() {
-  return <div className="footer-content">111</div>;
+  return (
+    <div className="footer-content">
+      <ExternalLink href="https://www.instagram.com/">
+        <img src={LogoInstagram} className="footer-img" />
+      </ExternalLink>
+      <ExternalLink href="https://www.facebook.com/">
+        <img src={LogoFacebook} className="footer-img" />
+      </ExternalLink>
+      ;
+    </div>
+  );
 }
